ff8854172bfe7d6e7a781f83f931efc9df0a9d9f
MateusSartorio <69646100+MateusSartorio@users.noreply.github.com>
2022-07-01 17:40:50 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define min(A, B) ( A < B ? A : B)
+#define max(A, B) ( A > B ? A : B)
+
 typedef struct string_t {
     char* c;
     int len;
@@ -20,11 +23,13 @@ void libera_vetor_sufixos(suffix* vet_suf);
 suffix* gera_sufixos(string* str);
 void libera_sufixos(suffix* s);
 void organiza_sufixos(suffix* s, int tam);
-void imprime_sufixos(suffix* s, int tam);
-void le_query(suffix* s);
-void query();
-int binary_search(suffix* vet_suf, int lo, int hi, string* k);
-int contem(string* s1, string* s2);
+void imprime_sufixos(suffix* vet_suf, int tam);
+void le_query(suffix* vet_suf, int tam, int contexto);
+void query(suffix* vet_suf, int tam, string* str, int contexto);
+int contem(string* s1, string* s2, int i1, int i2);
+int busca_binaria(suffix* vet_suf, int lo, int hi, string* k);
+void imprime_string(string* str);
+void imprime_string_intervalo(string* str, int i1, int i2);
 
 int main(int argc, char** argv) {
     FILE* f = fopen(argv[1], "r");
@@ -34,38 +39,21 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
+    int contexto = atoi(argv[2]);
+
     int n = 0;
     fscanf(f, "%d\n", &n);
 
     string* str = le_entrada(f, n);
+    // imprime_string(str);
 
-    suffix* s = gera_sufixos(str);
-    organiza_sufixos(s, str->len - 1);
-    // imprime_sufixos(s, str->len - 1);
+    suffix* vet_suf = gera_sufixos(str);
+    organiza_sufixos(vet_suf, str->len);
+    // imprime_sufixos(vet_suf, str->len);
 
-    le_query(s);
+    le_query(vet_suf, str->len, contexto);
 
-    // int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 32, 43, 94, 102, 305, 987};
-    // int i = binary_search(a, 0, 15, atoi(argv[2]));
-    // i >= 0 ? printf("%d\n", a[i]) : printf("Elemento nao encontrado!\n");
-
-    // char* s1 = "coo";
-    // char* s2 = "cool";
-
-    // contem(s1, s2, strlen(s1), strlen(s2)) ? printf("Contem!\n") : printf("Nao contem!\n");
-
-    // string* s1 = (string*) malloc(sizeof(string));
-    // s1->c = "A";
-    // s1->len = 7;
-
-    // string* s2 = (string*) malloc(sizeof(string));
-    // s2->c = "B";
-    // s2->len = 5;
-
-    // int i = compara_de(s1, s2, 0, 0);
-    // printf("%d\n", i);
-
-    libera_sufixos(s);
+    libera_sufixos(vet_suf);
     libera_string(str);
     fclose(f);
 
@@ -115,7 +103,7 @@ string* le_entrada(FILE* f, int n) {
         
         c_ant = c_atual;
     }
-    str->c[str->len++] = '\0';
+    str->c[str->len] = '\0';
 
     return str;
 }
@@ -130,9 +118,9 @@ void libera_vetor_sufixos(suffix* vet_suf) {
 }
 
 suffix* gera_sufixos(string* str) {
-    suffix*  vet_suf = (suffix*) malloc((str->len - 1)*sizeof(suffix));
+    suffix*  vet_suf = (suffix*) malloc(str->len*sizeof(suffix));
 
-    for(int i = 0; i < str->len - 1; i++) {
+    for(int i = 0; i < str->len; i++) {
         vet_suf[i].s = str;
         vet_suf[i].index = i;
     }
@@ -148,53 +136,104 @@ void organiza_sufixos(suffix* s, int tam) {
     qsort(s, tam, sizeof(suffix), compara_sufixos);
 }
 
-void imprime_sufixos(suffix* s, int tam) {
+void imprime_sufixos(suffix* vet_suf, int tam) {
     for(int i = 0; i < tam; i++)
-        printf("%s\n", s[i].s->c + s[i].index);
+        printf("%d: %s\n", i, vet_suf[i].s->c + vet_suf[i].index);
 }
 
-void le_query(suffix* s) {
+void le_query(suffix* vet_suf, int tam, int contexto) {
     char* linha = NULL;
-    int tam = 0;
+    int tam_lido = 0;
     size_t size = 0;
     
     string* str = (string*) malloc(sizeof(string));
-    while((tam = getline(&linha, &size, stdin)) > 1) {
-        printf("%d\n", tam);
-        str->c = linha;
-        str->len = tam - 1;
-        query(s, str);
+    while((tam_lido = getline(&linha, &size, stdin)) > 1) {
+        str->c = strtok(linha, "\n");
+        str->len = tam_lido - 1;
+        query(vet_suf, tam, str, contexto);
     }
 
     free(linha);
+    free(str);
 }
 
-void query(suffix* s, string* str) {
+void query(suffix* vet_suf, int tam, string* str, int contexto) {
+    int i = busca_binaria(vet_suf, 0, tam - 1, str);
 
+    if(i >= 0) {
+        printf("%s\n", vet_suf[i].s->c + vet_suf[i].index);
+        i++;
+
+        while((i < tam) && contem(vet_suf[0].s, str, vet_suf[i].index, 0)) {
+            imprime_string_intervalo(vet_suf[0].s, max(vet_suf[i].index - contexto, 0), min(vet_suf[i].index + contexto, tam - 1));
+            i++;
+        }
+    }
+    else
+        printf("Nao encontrado!\n");
 }
 
 //retorna 1 se s1 contem s2 a partir da origem, e 0 caso contrario
-int contem(string* s1, string* s2) {
-    if(s2->len > s1->len)
+int contem(string* s1, string* s2, int i1, int i2) {
+    if((s2->len - i2) > (s1->len - i1))
         return 0;
     
-    for(int i = 0; i < s2->len; i++)
-        if(s1->c[i] != s2->c[i])
+    for(int i = 0; i < (s2->len - i2); i++)
+        if(s1->c[i + i1] != s2->c[i + i2])
             return 0;
         
     return 1;
 }
 
-int binary_search(suffix* vet_suf, int lo, int hi, string* k) {
+int busca_binaria(suffix* vet_suf, int lo, int hi, string* k) {
     int mid = lo + (hi-lo)/2;
 
-    if((hi == lo) && (!contem(vet_suf[lo].s, k)))
+    // printf("================================================================\n");
+    // printf("lo: %d, mid: %d, hi: %d\n", lo, mid, hi);
+    // imprime_string(k);
+    // imprime_string(vet_suf[hi].s);
+    // int a = contem(vet_suf[hi].s, k,  vet_suf[hi].index, 0);
+    // if(a)
+    //     printf("Contem!\n");
+    // else
+    //     printf("Nao contem!\n");
+    // printf("================================================================\n");
+
+    if((hi <= lo) && (!contem(vet_suf[lo].s, k, vet_suf[lo].index, 0)))
         return -1;
 
-    if(contem(vet_suf[mid].s, k))
-        return mid;
-    else if(compara_de(vet_suf[mid].s, k, vet_suf[mid].index, 0) > 0)
-        return binary_search(vet_suf, lo, mid - 1, k);
+    if(contem(vet_suf[lo].s, k,  vet_suf[lo].index, 0))
+        return lo;
+
+    if(contem(vet_suf[mid].s, k,  vet_suf[mid].index, 0)) {
+        if(compara_de(vet_suf[mid].s, k, vet_suf[mid].index, 0) > 0)
+            return busca_binaria(vet_suf, lo + 1, mid, k);
+        else
+            return mid;
+    }        
+
+    if(compara_de(vet_suf[mid].s, k, vet_suf[mid].index, 0) > 0)
+        return busca_binaria(vet_suf, lo, mid - 1, k);
     else
-        return binary_search(vet_suf, mid + 1, hi, k);
+        return busca_binaria(vet_suf, mid + 1, hi, k);    
+}
+
+void imprime_string(string* str) {
+    printf("string: %s, tam: %d\n", str->c, str->len);
+}
+
+void imprime_string_intervalo(string* str, int i1, int i2) {
+    if(i1 < 0) {
+        printf("Indice menor que zero! - imprime_string_intervalo()\n");
+        exit(1);
+    }
+
+    if(i2 >= str->len) {
+        printf("Indice maior que o indice maximo - imprime_string_intervalo()\n");
+        exit(1);
+    }
+
+    for(int i = i1; i < i2; i++)
+        printf("%c", str->c[i]);
+    printf("\n");
 }
